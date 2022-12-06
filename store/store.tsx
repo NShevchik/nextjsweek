ca12c725688755d0be3ff5c69a93d38cdafca676
NShevchik <n.shevchik@softteco.com>
2022-12-06 12:42:26 +0100

    menu-changing-links

--- store/store.tsx
+++ store/store.tsx
@@ -1,11 +1,19 @@
 import create from 'zustand'
-
 interface MenuState {
     menu: string
     changeMenu: (menu: string) => void
+}
+interface UsersState<T> {
+    users: Array<T>
+    setUsers: (data: Array<T>) => void
 }
 
 export const useMenuOpen = create<MenuState>((set) => ({
     menu: 'translate-x-[-100%]',
     changeMenu: () => set((state) => ({ menu: state.menu === 'translate-x-[0]' ? 'translate-x-[-100%]' : 'translate-x-[0]' }))
 }))
+
+// export const useUsers = create<UsersState<Object>>((set) => ({
+//     users: [{ name: 'bebe', age: 22 }],
+//     setUsers: (data) => set(data)
+// }))
